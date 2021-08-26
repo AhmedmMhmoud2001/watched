206bd06e62cd28bee64cee5352920622528c6d19
Ahmedm Mahmoud <86148334+AhmedmMhmoud2001@users.noreply.github.com>
2021-08-26 21:57:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,68 @@
+// ------------------------- signup--------------------------------
+let fname=document.getElementById("fname");
+let email=document.getElementById("email");
+let password=document.getElementById("password");
+let sss =document.getElementById("sss");
+let yes= document.getElementById("yes");
+let error=document.getElementById("error");
+
+
+function myFunction(){
+    if(fname.value===""||fname.value==null||fname.value.length <= 5 ){
+        sss.style.display = "none";
+        error.innerHTML="Enter Your name min 5CH "
+    }
+    else if(email.value===""||email.value==null ){
+        sss.style.display = "none";
+        error.innerHTML="Enter Your email "
+    }
+    else if(password.value===""||password.value==null ||password.value.length <=15){
+        sss.style.display = "none";
+        error.innerHTML="Enter Your password min 15 "
+    }
+    else if(password.value.length > 15){
+        sss.style.display = "block";
+        error.innerHTML=" "
+    }
+    else
+    sss.style.display = "block";
+
+    if( password.value.length > 3){
+        document.getElementById("span1").style.backgroundColor = "red";
+       
+   }
+   if( password.value.length >6){
+    document.getElementById("span1").style.backgroundColor = "yellow";
+       document.getElementById("span2").style.backgroundColor = "yellow";
+      
+   }
+   if(password.value.length >=10){
+    document.getElementById("span1").style.backgroundColor = "green";
+    document.getElementById("span2").style.backgroundColor = "green";
+    document.getElementById("span3").style.backgroundColor = "green";
+      
+   }
+   if(password.value.length >=15){
+    document.getElementById("span1").style.backgroundColor = "green";
+    document.getElementById("span2").style.backgroundColor = "green";
+    document.getElementById("span3").style.backgroundColor = "green";
+    document.getElementById("span4").style.backgroundColor = "green";
+    }
+};
+
+
+// ------------------------------index2--------------------------------
+
 var API_Key="api_key=b14e0d0a5744eadbded54b795209fc69";
 var BASE_URL = "https://api.themoviedb.org/3";
 var API_URL = BASE_URL+"/discover/movie?sort_by=popularity.desc&"+ API_Key;
-
 var IMG_URL = "https://image.tmdb.org/t/p/w500";
 var searchURL ="https://api.themoviedb.org/3/search/movie?api_key=b14e0d0a5744eadbded54b795209fc69";
-
-
-
-
 const form =document.getElementById("form");
 const search=document.getElementById("search");
 const main=document.getElementById("main");
 
 
-
-
-// function getMovies(url){
-//     fetch(url).then(res => res.json()).then(data =>{
-//     console.log(data);
-//     showMovies(data.results);
-//     })
-// }
 
 
 
@@ -30,27 +72,6 @@ function getMovies(url){
     showMovies(data.results);
     })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 function showMovies(movies){
@@ -82,19 +103,6 @@ function showMovies(movies){
     });
 }
 
-// function showMovies(movies){
-//     main.innerHTML="";
-//     movies.forEach(movie => {
-//     const { poster_path ,title,vote_average,overview}=movie;
-//     const movieEL = document.createElement("div");
-//     movieEL.classList.add("movie");
-//     movieEL.innerHTML =`
-//     <img src="${IMG_URL +poster_path }" alt="${title}" />
-//     `
-//     main.appendChild(movieEL);
-//     });
-
-// }
 getMovies(API_URL);
 function getClassByRate(vote){
     if(vote >= 8){
